0530c42c90bc0569535af75cf1efe87247fb7b27
deepaknairrpf <cse15b016@iiitk.ac.in>
2016-06-17 00:44:45 +0530

    Add files via upload

---
+++ asgnmt_7_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class asgnmt_7_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+		Scanner sc=null;
+		sc.nextInt();
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println(e + " \nScanner object is assigned to null instead of being initialized");
+		}
+	}
+
+}
